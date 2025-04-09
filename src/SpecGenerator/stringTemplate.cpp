c6dc2271a0af42a85428b02326e821d940b975e3
SgtPepper114 <1992117008@qq.com>
2025-04-09 10:58:37 +0800

    format&fix StringTemplate::operator()

--- src/SpecGenerator/stringTemplate.cpp
+++ src/SpecGenerator/stringTemplate.cpp
@@ -3,31 +3,38 @@
 using namespace std;
 
 // Initialize the instance with rawText_, nested $ will be ignored.
-void StringTemplate::initialize(){
+void StringTemplate::initialize()
+{
     Placeholder curPH; // PH = placeholder
     bool afterDollor = false;
     bool inBraces = false;
-    for(int i = 0; i < rawText_.size(); ++i){
+    for(int i = 0; i < rawText_.size(); ++i)
+    {
         char ch = rawText_[i];
-        
-        if(inBraces){
+
+        if(inBraces)
+        {
             ++curPH.len;
-            if(ch == '}'){
+            if(ch == '}')
+            {
                 placeholders_.push_back(curPH);
                 inBraces = false;
-            } else {
+            }
+            else
+            {
                 curPH.name += ch;
             }
             continue;
         }
 
-        if(afterDollor && ch == '{'){
+        if(afterDollor && ch == '{')
+        {
             inBraces = true;
             curPH.name = "";
             curPH.pos = i;
             curPH.len = 2;
         }
-        
+
         afterDollor = (ch == '$');
     }
 }
@@ -35,45 +42,54 @@ void StringTemplate::initialize(){
 // Replace placeholders named ${key} with ${value}.
 // With mp has both <A, B> and <B, C>, the placeholders named A will have name B eventually.
 // /return the total number of replaced placeholders.
-size_t StringTemplate::remap(const unordered_map<string, string>& mp){
+size_t StringTemplate::remap(const unordered_map<string, string> &mp)
+{
     size_t count = 0;
     unordered_map<string, unordered_set<size_t>> temp;
-    for(auto& kv : mp){
+    for(auto &kv : mp)
+    {
         if(!name2PH_.count(kv.first))
             continue;
 
-        for(auto& id : name2PH_[kv.first]){
+        for(auto &id : name2PH_[kv.first])
+        {
             placeholders_[id].name = kv.second;
             ++count;
         }
         temp[kv.second] = std::move(name2PH_[kv.first]);
         name2PH_.erase(kv.first);
     }
-    for(auto& kv : temp){
+    for(auto &kv : temp)
+    {
         name2PH_[kv.first].merge(std::move(kv.second));
     }
 
-    if(next_) count += next_->remap(mp);
+    if(next_)
+        count += next_->remap(mp);
     return count;
 }
 
 // prefix all placeholders' name
-void StringTemplate::withPrefix(const string& prefix){
-    for(auto& ph : placeholders_){
+void StringTemplate::withPrefix(const string &prefix)
+{
+    for(auto &ph : placeholders_)
+    {
         ph.name = prefix + ph.name;
     }
     unordered_map<string, unordered_set<size_t>> temp;
-    for(auto& kv : name2PH_){
+    for(auto &kv : name2PH_)
+    {
         temp[prefix + kv.first] = std::move(kv.second);
     }
     name2PH_ = std::move(temp);
 
-    if(next_) next_->withPrefix(prefix);
+    if(next_)
+        next_->withPrefix(prefix);
 }
 
 // In terms of frequent concatenations.
-template <typename T, typename>
-void StringTemplate::append(T&& templ){
+template <typename T, typename> void StringTemplate::append(T &&templ)
+{
     if(next_)
         next_->append(forward<T>(templ));
     else
@@ -81,34 +97,42 @@ void StringTemplate::append(T&& templ){
 }
 
 // /return the string with all placeholders replaced by mp
-string StringTemplate::operator() (const unordered_map<string, string>& mp) const{
+string StringTemplate::operator()(const unordered_map<string, string> &mp) const
+{
     string result;
     size_t curPos = 0;
-    for(auto& ph : placeholders_){
-        result += rawText_.substr(curPos, ph.pos-curPos);
+    for(auto &ph : placeholders_)
+    {
+        result += rawText_.substr(curPos, ph.pos - curPos);
         if(mp.count(ph.name))
             result += mp.at(ph.name);
         curPos = ph.pos + ph.len;
     }
+    if(curPos < rawText_.length())
+        result += rawText_.substr(curPos);
 
-    if(next_) result += (*next_)(mp);
+    if(next_)
+        result += (*next_)(mp);
     return result;
 }
 
 // Output string with placeholders replaced by mp to os.
-void StringTemplate::operator() (ostream& os, const unordered_map<string, string>& mp) const{
+void StringTemplate::operator()(ostream &os, const unordered_map<string, string> &mp) const
+{
     size_t curPos = 0;
-    for(auto& ph : placeholders_){
-        os << rawText_.substr(curPos, ph.pos-curPos);
+    for(auto &ph : placeholders_)
+    {
+        os << rawText_.substr(curPos, ph.pos - curPos);
         if(mp.count(ph.name))
             os << mp.at(ph.name);
         curPos = ph.pos + ph.len;
     }
+    if(curPos < rawText_.length())
+        os << rawText_.substr(curPos);
 
-    if(next_) (*next_)(os, mp);
+    if(next_)
+        (*next_)(os, mp);
 }
 
 // String literal to StringTemplate.
-StringTemplate operator"" _ST(const char* str){
-    return StringTemplate(str);
-}
+StringTemplate operator"" _ST(const char *str) { return StringTemplate(str); }
--- src/SpecGenerator/stringTemplate.h
+++ src/SpecGenerator/stringTemplate.h
@@ -14,11 +14,13 @@
 
 using namespace std;
 
-class StringTemplate {
-private:
+class StringTemplate
+{
+  private:
     string rawText_;
 
-    struct Placeholder {
+    struct Placeholder
+    {
         string name;
         size_t pos;
         size_t len;
@@ -27,44 +29,42 @@ private:
     unordered_map<string,
         unordered_set<size_t>> name2PH_; // PH = placeholder
     unique_ptr<StringTemplate> next_;
-        
+
     void initialize();
 
-public:
+  public:
     StringTemplate() = delete;
     // Has same behavior with nullptr and empty string.
-    StringTemplate(const char* str) {
+    StringTemplate(const char *str)
+    {
         if(str == nullptr)
             rawText_ = "";
         initialize();
     };
-    StringTemplate(const string& str) : rawText_(str) { initialize(); }
+    StringTemplate(const string &str) : rawText_(str) { initialize(); }
 
     size_t getPlaceholderNum() const { return placeholders_.size(); }
-    void withPrefix(const string&);
-    size_t remap(const unordered_map<string, string>&);
+    void withPrefix(const string &);
+    size_t remap(const unordered_map<string, string> &);
 
     template <typename T,
         typename = std::enable_if_t<
-            std::is_same_v<std::remove_cv_t<std::remove_reference_t<T>>, StringTemplate>
-        >
-    >
-    void append(T&&);
-    
+            std::is_same_v<std::remove_cv_t<std::remove_reference_t<T>>, StringTemplate>>>
+    void append(T &&);
+
     template <typename T,
         typename = std::enable_if_t<
-            std::is_same_v<std::remove_cv_t<std::remove_reference_t<T>>, StringTemplate>
-        >
-    >
-    friend StringTemplate operator+  (T&& templA, T&& templB){
-        auto& temp = templA;
+            std::is_same_v<std::remove_cv_t<std::remove_reference_t<T>>, StringTemplate>>>
+    friend StringTemplate operator+(T &&templA, T &&templB)
+    {
+        auto &temp = templA;
         temp.append(forward<T>(templB));
         return temp;
     }
-    string operator() (const unordered_map<string, string>&) const;
-    void operator() (ostream&, const unordered_map<string, string>&) const;
+    string operator()(const unordered_map<string, string> &) const;
+    void operator()(ostream &, const unordered_map<string, string> &) const;
 };
 
-StringTemplate operator"" _ST(const char*);
+StringTemplate operator"" _ST(const char *);
 
 #endif
