858e41bd64cabe826b6cfb5e31a0383f6e05fb52
SgtPepper114 <1992117008@qq.com>
2025-04-16 16:30:46 +0800

    TEST: add PathTest

--- tests/analyzer/state_test.cpp
+++ tests/analyzer/state_test.cpp
@@ -11,21 +11,8 @@
 using ::testing::Return;
 using namespace std;
 
-class Test
+TEST(PathTest, DumpEmptyPath)
 {
-  public:
-    int foo(int x) { return x; }
-};
-
-class MockTest : public Test
-{
-  public:
-    MOCK_NONVIRTUAL_METHOD(int, foo, (int), (), MockTest);
-};
-
-TEST(Test, test)
-{
-    MockTest testA;
-    EXPECT_CALL(testA, foo).WillRepeatedly(Return(0));
-    EXPECT_EQ(testA.foo(1), 0);
+    Path path;
+    EXPECT_NO_THROW(path.dump());
 }
