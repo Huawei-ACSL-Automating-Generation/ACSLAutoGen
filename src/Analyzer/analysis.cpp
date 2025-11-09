679cde9d1bf94e7d60d1c20ea579074b645e12f6
SgtPepper114 <1992117008@qq.com>
2025-11-09 14:19:16 +0000

    A change to support generating ACSL inside main function. * Feat: when meeting main function, `generateFunctionSpec` steps the function body but skip the generating of function contracts.

--- src/Analyzer/analysis.cpp
+++ src/Analyzer/analysis.cpp
@@ -24,10 +24,6 @@ namespace acslg::analyzer {
     void ACSLAnalyzer::generateFunctionSpec(ACSLFunction *func) {
         const clang::FunctionDecl *FD = func->getFunctionDecl();
 
-        if (FD->getNameAsString() == "main") {
-            WARN("Ignore MAIN Function.");
-            return;
-        }
         // @WindOctober: TODO remove.
         // if (FD->getNameAsString() != "BN_Create" && FD->getNameAsString() != "BN_SetFlag" &&
         //     FD->getNameAsString() != "BN_IsZero")
@@ -36,42 +32,34 @@ namespace acslg::analyzer {
 
         auto state = std::make_unique<ProgramState>(std::make_unique<ACSLFunction>(FD), context_);
 
-        if (const clang::Stmt *Body = FD->getBody()) {
-            if (!isa<clang::CompoundStmt>(Body))
-                UNIMPLEMENT("Function body of " + FD->getNameAsString() +
-                            " is not a clang::CompoundStmt");
-
-            state->init();
-            const clang::CompoundStmt *CS = cast<clang::CompoundStmt>(Body);
-            auto preState                 = state->clone();
-
-            for (const clang::Stmt *stmt : CS->children())
-                state->step(stmt);
-            INFO(state->dump());
-
-            bool hasPointer = false, hasLoop = false;
-            for (auto &&[_, value] : preState->getPaths()[0]->getMemoryState().flat()) {
-                if (llvm::isa<symbolic::Address>(*value))
-                    hasPointer = true;
-            }
-            for (auto stmt : dyn_cast<clang::CompoundStmt>(Body)->children()) {
-                if (isa<clang::ForStmt>(stmt) || isa<clang::WhileStmt>(stmt) ||
-                    isa<clang::DoStmt>(stmt))
-                    hasLoop = true;
-            }
-
-            if (hasPointer && hasLoop)
-                return;
-
-            auto [spec, usedPoints] = spec_generator::emitFunctionContract(*preState, *state);
-            INFO(spec);
-
-            auto &SM       = context_.getSourceManager();
-            auto fileBegin = SM.getFileLoc(FD->getSourceRange().getBegin());
-            context_.insertText(fileBegin, spec, /*after*/ false, /*indentNewLines*/ true);
-            context_.insertUsedPoints(std::move(usedPoints));
-        } else {
+        const clang::Stmt *body = FD->getBody();
+        if (body == nullptr) {
             INFO("No function body found for: " + FD->getNameAsString());
+            return;
         }
+        if (!isa<clang::CompoundStmt>(body))
+            UNIMPLEMENT("Function body of " + FD->getNameAsString() +
+                        " is not a clang::CompoundStmt");
+
+        state->init();
+        const clang::CompoundStmt *CS = cast<clang::CompoundStmt>(body);
+        auto preState                 = state->clone();
+
+        for (const clang::Stmt *stmt : CS->children())
+            state->step(stmt);
+        INFO(state->dump());
+
+        if (FD->getNameAsString() == "main") {
+            WARN("Ignore the contracts generating of MAIN Function.");
+            return;
+        }
+
+        auto [spec, usedPoints] = spec_generator::emitFunctionContract(*preState, *state);
+        INFO(spec);
+
+        auto &SM       = context_.getSourceManager();
+        auto fileBegin = SM.getFileLoc(FD->getSourceRange().getBegin());
+        context_.insertText(fileBegin, spec, /*after*/ false, /*indentNewLines*/ true);
+        context_.insertUsedPoints(std::move(usedPoints));
     }
 } // namespace acslg::analyzer
