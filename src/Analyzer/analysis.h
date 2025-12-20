22b6490303d821ec4be88a7999f76c0751e900b5
WindOctober <Windocotber@gmail.com>
2025-12-20 09:25:41 +0000

    Impl: Add --func selector for single-function analysis Revise: Filter analyzer traversal to the requested function

--- src/Analyzer/analysis.h
+++ src/Analyzer/analysis.h
@@ -9,13 +9,15 @@
 namespace acslg::analyzer {
     class ACSLAnalyzer {
       public:
-        ACSLAnalyzer(context::ACSLGContext &ctx) : context_(ctx) {}
+        ACSLAnalyzer(context::ACSLGContext &ctx, std::string targetFuncName = {})
+            : context_(ctx), targetFuncName_(std::move(targetFuncName)) {}
 
         void analyzeFunctions();
 
       private:
         context::ACSLGContext &context_;
         std::vector<std::unique_ptr<ACSLFunction>> functions_;
+        std::string targetFuncName_;
 
         void generateFunctionSpec(ACSLFunction *func);
     };
