cbfffc6a6fd21e8d79f1f889cea4d0fb18efc087
SgtPepper114 <1992117008@qq.com>
2026-07-06 03:19:43 +0000

    Import test helper expression clones through factory

--- tests/testUtils/testHelper.cpp
+++ tests/testUtils/testHelper.cpp
@@ -108,7 +108,8 @@ namespace acslg::test::utils {
         auto &returnExpr = firstPath->getReturnExpr();
         if (returnExpr == nullopt)
             ERROR("There is no returnExpr!");
-        return returnExpr.value()->clone();
+        auto &factory = state.getExprFactory();
+        return factory.cloneExpr(factory.importExpr(*returnExpr.value()));
     }
 
     not_null<unique_ptr<ProgramState>> getPostStateOfFirstLoop(const string_view code) {
@@ -311,12 +312,12 @@ namespace acslg::test::utils {
     }
 
     not_null<unique_ptr<symbolic::SymbolicExpr>> FixtureWithCode::makeLiteralExpr(uint64_t value) {
-        return exprFactory_.literal(value)->clone();
+        return exprFactory_.cloneExpr(exprFactory_.literal(value));
     }
 
     not_null<unique_ptr<symbolic::SymbolicExpr>> FixtureWithCode::makeRangeIndexExpr(
         string_view name) {
-        return exprFactory_.rangeIndex(name)->clone();
+        return exprFactory_.cloneExpr(exprFactory_.rangeIndex(name));
     }
 
     symbolic::SymbolAddress FixtureWithCode::makeRangeAddr(
